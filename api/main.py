5ec1cb88e65a77a1e92a26e9a2cc5ba618a0595f
ysk91 <ysk91.engineer@gmail.com>
2024-12-10 22:07:26 +0900

    routerを設定

--- api/main.py
+++ api/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from api.routers import tasks, done
+
 app = FastAPI()
-
-
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world!"}
+app.include_router(tasks.router)
+app.include_router(done.router)
