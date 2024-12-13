a34f3f47e0594413d9b6e80f5cd20b98c253580e
ysk91 <ysk91.engineer@gmail.com>
2024-12-13 18:36:33 +0900

    postを実装

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ https://zenn.dev/sh0nk/books/537bb028709ab9
 ## 環境構築
 
 ```shell
+# イメージの作成
 $ docker-compose build
+# pyprojectに含まれるパッケージのインストール
+$ docker-compose run --entrypoint "poetry install --no-root" demo-app
+# 再度ビルド
+$ docker-compose build --no-cache
+# コンテナの起動
 $ docker-compose up -d
 # コンテナに入る
 $ docker-compose exec demo-app bash
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -3,8 +3,32 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
+# スキーマの共通部分を TaskBase として定義
+# 継承元のBaseModelはPydanticの基底クラスで、FastAPIのスキーマモデルであることを表す
+class TaskBase(BaseModel):
     # カラム名: 型ヒント  Field(デフォルト値, その他) という形式で記述
-    id: int
     title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+# GET /tasks で返すレスポンスのスキーマ
+# TaskBaseを継承して、idとdoneを追加
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
+
+
+# POST /tasks で受け取るリクエストのスキーマ
+class TaskCreate(TaskBase):
+    pass
+
+
+# POST /tasksした後のレスポンスのスキーマ
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+
+    class Config:
+        orm_mode = True
