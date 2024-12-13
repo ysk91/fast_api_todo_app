0c7cef81062ddd967a8dd594eb594205e9f032d5
ysk91 <ysk91.engineer@gmail.com>
2024-12-13 18:49:15 +0900

    doneを実装

--- api/routers/done.py
+++ api/routers/done.py
@@ -4,10 +4,10 @@ router = APIRouter()
 
 
 @router.put("/tasks/{task_id}/done")
-async def mark_task_as_done():
-    pass
+async def mark_task_as_done(task_id: int):
+    return
 
 
 @router.delete("/tasks/{task_id}/done")
-async def mark_task_as_not_done():
-    pass
+async def mark_task_as_not_done(task_id: int):
+    return
