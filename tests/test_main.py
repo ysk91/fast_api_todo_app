6002845eda7c9065b2af258d57bd338cd429268e
ysk91 <ysk91.engineer@gmail.com>
2024-12-15 22:10:20 +0900

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import pytest
 from httpx import AsyncClient
+import starlette.status
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
@@ -32,3 +33,45 @@ async def async_client() -> AsyncClient:
     # テスト用に非同期HTTPクライアントを返却
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
+
+
+@pytest.mark.asyncio
+async def test_create_and_read(async_client):
+    # post /tasksで新規タスクが作成されること
+    response = await async_client.post("/tasks", json={"title": "テストタスク"})
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert response_obj["title"] == "テストタスク"
+
+    # get /tasksで作成したタスクが取得できること
+    response = await async_client.get("/tasks")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert len(response_obj) == 1
+    assert response_obj[0]["title"] == "テストタスク"
+    assert response_obj[0]["done"] is False
+
+
+@pytest.mark.asyncio
+async def test_done_flag(async_client):
+    # post /tasksで新規タスクが作成されること
+    response = await async_client.post("/tasks", json={"title": "テストタスク2"})
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert response_obj["title"] == "テストタスク2"
+
+    # put /tasks/1/doneでdoneフラグを更新できること
+    response = await async_client.put("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_200_OK
+
+    # すでにdoneフラグがTrueの場合は400エラーが返却されること
+    response = await async_client.put("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_400_BAD_REQUEST
+
+    # delete /tasks/1/doneでdoneフラグを削除できること
+    response = await async_client.delete("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_200_OK
+
+    # すでにdoneフラグがFalseの場合は400エラーが返却されること
+    response = await async_client.delete("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_404_NOT_FOUND
